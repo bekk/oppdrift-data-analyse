0250f2d4f3ce5d27260a796cf0e63578f6e83a61
August Heiervang Dahl <augusthdahl@gmail.com>
2026-03-10 12:34:45 +0100

    Fix typo in custom schema name check

--- del2/dbt/macros/get_custom_schema.sql
+++ del2/dbt/macros/get_custom_schema.sql
@@ -1,6 +1,6 @@
 {% macro generate_schema_name(custom_schema_name, node) -%}
   {%- set default_schema = target.schema -%}
-  {%- if custome_schema_name is none -%}
+  {%- if custom_schema_name is none -%}
     {{ default_schema }}
   {%- else -%}
     {{ custom_schema_name | trim }}
